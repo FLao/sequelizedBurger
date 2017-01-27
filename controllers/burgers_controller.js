dff058845064c60fbd450b34feb31945085adf08
Francis Ian Lao <Francis Ian Lao>
2017-01-27 14:55:23 -0800

    added partial schema code to initialize database

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -7,7 +7,6 @@ var db = require("../models");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(request, response) {
-  // Add sequelize code to find all posts, and return them to the user with res.json
   db.Burger.findAll({}).then(function(dbBurger) {
     // We have access to the todos as an argument inside of the callback function
     var hbsObject = {
@@ -28,9 +27,6 @@ router.post("/", function(request, response) {
 });
 
 router.put("/:id", function(request, response) {
-
-//  console.log("here!!!");
-
   db.Burger.update({devoured: 1},
     {
       where: {
